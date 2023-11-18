dd32d3ad622b730ded91b643c72414c4bb8b7035
accuman <rt0107@mail.ru>
2023-11-18 13:00:13 +0300

    Update usb-mount.sh

--- usb-mount.sh
+++ usb-mount.sh
@@ -9,18 +9,33 @@
 # This script is called from systemd unit file to mount or unmount
 # a USB drive.
 
-PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
-log="logger -t usb-mount.sh -s "
+# PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
+
+######################################################################
+# Auxiliary functions
+
+# Log a string via the syslog facility.
+log()
+{
+    if [ $1 != debug ] || expr "$VERBOSE" : "[yY]" > /dev/null; then
+        logger -p user.$1 -t "usb-mount[$$]" -- "$2"
+    fi
+}
 
 usage()
 {
-    ${log} "Usage: $0 {add|remove} device_name (e.g. sdb1)"
+    log info "Usage: $0 {add|remove} device_name (e.g. sdb1)"
     exit 1
 }
 
 if [[ $# -ne 2 ]]; then
     usage
 fi
+
+######################################################################
+# Main program
+
+VERBOSE=yes
 
 ACTION=$1
 DEVBASE=$2
@@ -29,69 +44,48 @@ DEVICE="/dev/${DEVBASE}"
 # See if this drive is already mounted, and if so where
 MOUNT_POINT=$(mount | grep ${DEVICE} | awk '{ print $3 }')
 
-DEV_LABEL=""
-
 do_mount()
 {
     if [[ -n ${MOUNT_POINT} ]]; then
-        ${log} "Warning: ${DEVICE} is already mounted at ${MOUNT_POINT}"
+        log error "Warning: ${DEVICE} is already mounted at ${MOUNT_POINT}"
         exit 1
     fi
 
-    # Get info for this drive: $ID_FS_LABEL and $ID_FS_TYPE
-    eval $(blkid -o udev ${DEVICE} | grep -i -e "ID_FS_LABEL" -e "ID_FS_TYPE")
+    MOUNT_POINT="/media/TRANSCEND"
+    log debug "Mount point: ${MOUNT_POINT}"
 
-    # Figure out a mount point to use
-    LABEL=${ID_FS_LABEL}
-    if grep -q " /media/${LABEL} " /etc/mtab; then
-        # Already in use, make a unique one
-        LABEL+="-${DEVBASE}"
+    if grep -q " ${MOUNT_POINT} " /etc/mtab; then
+        log error "Mount point ${MOUNT_POINT} already in use, make an unique one"
+	exit 1
+#        log info "Mount point ${MOUNT_POINT} already in use, make an unique one"
+#        MOUNT_POINT+="-${DEVBASE}"
     fi
-    DEV_LABEL="${LABEL}"
-
-    # Use the device name in case the drive doesn't have label
-    if [ -z ${DEV_LABEL} ]; then
-        DEV_LABEL="${DEVBASE}"
-    fi
-
-    MOUNT_POINT="/media/${DEV_LABEL}"
-
-    ${log} "Mount point: ${MOUNT_POINT}"
-
     mkdir -p ${MOUNT_POINT}
 
     # Global mount options
     OPTS="rw,relatime"
 
-    # File system type specific mount options
-    if [[ ${ID_FS_TYPE} == "vfat" ]]; then
-        OPTS+=",users,gid=100,umask=000,shortname=mixed,utf8=1,flush"
-    fi
-
     if ! mount -o ${OPTS} ${DEVICE} ${MOUNT_POINT}; then
-        ${log} "Error mounting ${DEVICE} (status = $?)"
-        rmdir "${MOUNT_POINT}"
+        log error "Error mounting ${DEVICE} (status = $?)"
         exit 1
     else
         # Track the mounted drives
         echo "${MOUNT_POINT}:${DEVBASE}" | cat >> "/var/log/usb-mount.track" 
     fi
 
-    ${log} "Mounted ${DEVICE} at ${MOUNT_POINT}"
+    log info "Mounted ${DEVICE} at ${MOUNT_POINT}"
 }
 
 do_unmount()
 {
     if [[ -z ${MOUNT_POINT} ]]; then
-        ${log} "Warning: ${DEVICE} is not mounted"
+        log error "Warning: ${DEVICE} is not mounted"
+	exit 1
     else
         umount -l ${DEVICE}
-	${log} "Unmounted ${DEVICE} from ${MOUNT_POINT}"
-        /bin/rmdir "${MOUNT_POINT}"
+	log info "Unmounted ${DEVICE} from ${MOUNT_POINT}"
         sed -i.bak "\@${MOUNT_POINT}@d" /var/log/usb-mount.track
     fi
-
-
 }
 
 case "${ACTION}" in
