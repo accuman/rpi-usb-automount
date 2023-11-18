c9f42244b9671a96d83956f3c18ce549488d7369
accuman <rt0107@mail.ru>
2023-11-18 13:04:58 +0300

    Update REMOVE.sh

--- REMOVE.sh
+++ REMOVE.sh
@@ -5,17 +5,18 @@
 PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
 
 # Just a best effort
-rm -f /usr/local/bin/usb-mount.sh
+sudo rm -f /usr/local/bin/usb-mount.sh
 
 # Systemd unit file for USB automount/unmount 
-rm -f /etc/systemd/system/usb-mount@.service
+sudo rm -f /etc/systemd/system/usb-mount@.service
 
 # Remove the track files
-rm -f /var/log/usb-mount.track*
+sudo rm -f /var/log/usb-mount.track*
 
 # Remove udev rule
-sed -i "/systemctl\sstart\susb-mount/d" /etc/udev/rules.d/99-local.rules
-sed -i "/systemctl\sstop\susb-mount/d" /etc/udev/rules.d/99-local.rules
+sudo sed -i "/systemctl\sstart\susb-mount/d" /etc/udev/rules.d/99-local.rules
+sudo sed -i "/systemctl\sstop\susb-mount/d" /etc/udev/rules.d/99-local.rules
 
-systemctl daemon-reload
-udevadm control --reload-rules
+sudo systemctl daemon-reload
+sudo udevadm control --reload-rules
+sudo service udev restart
