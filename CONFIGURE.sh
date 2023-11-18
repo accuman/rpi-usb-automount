6f0ab8ee86428f1534c050200c2764591e95b08c
accuman <rt0107@mail.ru>
2023-11-18 13:02:31 +0300

    Update CONFIGURE.sh

--- CONFIGURE.sh
+++ CONFIGURE.sh
@@ -2,16 +2,17 @@
 # If you are executing this script in cron with a restricted environment,
 # modify the shebang to specify appropriate path; /bin/bash in most distros.
 # And, also if you aren't comfortable using(abuse?) env command.
-PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
-chmod 755 ./*.sh
+#PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
 
-cp ./usb-mount.sh /usr/local/bin/
+sudo cp ./usb-mount.sh /usr/local/bin/
+sudo chmod 755 /usr/local/bin/usb-mount.sh
 
 # Systemd unit file for USB automount/unmount 
-cp ./usb-mount@.service /etc/systemd/system/usb-mount@.service
+sudo cp ./usb-mount@.service /etc/systemd/system/usb-mount@.service
 
 # Create udev rule to start/stop usb-mount@.service on hotplug/unplug
-cat ./99-local.rules.usb-mount >> /etc/udev/rules.d/99-local.rules
+sudo cat ./99-local.rules.usb-mount >> /etc/udev/rules.d/99-local.rules
 
-systemctl daemon-reload
-udevadm control --reload-rules
+sudo systemctl daemon-reload
+sudo udevadm control --reload-rules
+sudo service udev restart
